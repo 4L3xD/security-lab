f48dc71738ae390047f8ad23caf539f7d5a1ed45
4L3xD <alessandra.olea@gmail.com>
2023-10-24 10:50:17 -0300

    add explanation about the vulnerable code

--- SSRF/var/www/TestApp/app/views.py
+++ SSRF/var/www/TestApp/app/views.py
@@ -7,35 +7,44 @@ def home():
     return render_template("home.html")
 
 
+#################### CÓDIGO VULNERÁVEL ####################
+'''Parâmetros controlados pelo usuário permitem requisições maliciosas
+para recursos internos ou para aplicações externas.'''
+
 @app.route("/param/<param_name>")
 def param_vuln(param_name):
-    # BAD:
-    # user-controlled parameters can allow an attacker to
-    # to send malicious requests to internal resources
-    # or other external applications
-    
-    # EX: localhost:56733/service_status/google.com
     return redirect(f"https://{param_name}.com")
-
-    # GOOD:
-    # It is highly recommend to use a whitelist approach
-
-    # if service_name == "someservice":
-    #     return requests.get("https://someservice.com/myrequest")
-    # elif service_name == "otherservice":
-    #     return requests.get("https://otherservice.com/otherrequest")
-    # else:
-    #     return requests.get("https://defaultservice.com/defaultrequest")
-
+    # Exploit: localhost:56733/param/google
 
 @app.route("/args")
 def arg_vuln():
     target = request.args["target"]
-
-    # BAD: user has full control of URL
-    # EX: localhost:56733/full_ssrf?target=google.com?
     return redirect(f"https://{target}.example.com/data/")
+    # Exploit: localhost:56733/args?target=google.com?
 
-    # # GOOD: `subdomain` is controlled by the server.
-    # subdomain = "europe" if target == "EU" else "world"
-    # resp = requests.get("https://" + subdomain + ".example.com/data/")
+###########################################################
+
+
+#################### CÓDIGO CORRIGIDO #####################
+
+'''É recomendado o uso de whitelist'''
+
+# @app.route("/param/<param_name>")
+# def param_vuln(param_name):
+#     if param_name == "servico-1":
+#         return redirect("https://servico-1.com/exemplo1")
+#     elif param_name == "servico-2":
+#         return redirect("https://servico-2.com/exemplo2")
+#     else:
+#         return redirect("https://servico-padrao.com/requisicao-padrao")
+
+
+'''O subdomínio é controlado pelo servidor'''
+
+# @app.route("/args")
+# def arg_vuln():
+#     target = request.args["target"]
+#     subdomain = "brasil" if target == "BR" else "mundo"
+#     return redirect(f"https://{subdomain}.example.com/data/")
+
+###########################################################
